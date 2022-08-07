cbeab321ab7f0c7750dd5b99fdbde89a41b139f2
pbull <17122363+peterbull@users.noreply.github.com>
2022-08-07 08:48:27 -0400

    Added Wall deleting transaction

--- sheet_param.py
+++ sheet_param.py
@@ -1,14 +1,22 @@
 __title__ = ""
 __author__ = "Peter Bull"
 
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, Transaction, TransactionGroup
 
 doc =  __revit__.ActiveUIDocument.Document
 
-# Creating collector instance and collecting all the walls from the model
+# Creating collector instance and collecting all the sheets from the model
 sheets_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Sheets)\
                                                     .WhereElementIsNotElementType()\
                                                     .ToElements()
+
+# Creating collector instance and collecting all wall element ids in the model
+wall_id_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Walls)\
+                                                 .WhereElementIsNotElementType()\
+                                                 .ToElementIds()
+
+tg = TransactionGroup(doc, "Update and Delete")
+tg.Start()
 
 t = Transaction(doc, "Update Sheet Parameters")
 t.Start()
@@ -19,3 +27,17 @@ for sheet in sheets_collector:
         custom_param.Set("Example Value")
 
 t.Commit()
+
+
+
+
+t = Transaction(doc, "Delete All Walls")
+
+t.Start()
+
+for wall_id in wall_id_collector:
+    doc.Delete(wall_id)
+
+t.Commit()
+
+tg.Assimilate()
