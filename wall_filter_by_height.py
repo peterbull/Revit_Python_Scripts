1c1f491325a3774ca864b68dba82e3bb69dd91f8
pbull <17122363+peterbull@users.noreply.github.com>
2022-08-08 19:01:08 -0400

    Diff check

--- wall_filter_by_height.py
+++ wall_filter_by_height.py
@@ -1,25 +1,24 @@
-"""
-Advanced collection of data: Collect all the walls at height of 10
-by iterating over all walls in project and checking parameter
-"""
-
 from System.Collections.Generic import List
 
 import Autodesk.Revit.DB as DB
 
-doc = __revit__.activeUIDocument.Document
-uidoc = __revit__.activeUIDocument
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+
 
 walls = DB.FilteredElementCollector(doc) \
-        .OfCategory(DB.BuiltInCategory.OST_Walls) \
-        .WhereElementIsNotElementType() \
-        .ToElements()
+          .OfCategory(DB.BuiltInCategory.OST_Walls) \
+          .WhereElementIsNotElementType() \
+          .ToElements()
+
 
 tallwalls_ids = []
 
 for wall in walls:
-    heightp = wall.LookupParameter('Unconnected Height')
+    heightp = wall.Parameter[DB.BuiltInParameter.WALL_USER_HEIGHT_PARAM]
     if heightp and heightp.AsDouble() == 10.0:
         tallwalls_ids.append(wall.Id)
+
 
 uidoc.Selection.SetElementIds(List[DB.ElementId](tallwalls_ids))
