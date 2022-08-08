c12cf53bafc36f90b52e455b923467b094cce878
pbull <17122363+peterbull@users.noreply.github.com>
2022-08-08 19:00:01 -0400

    Troubleshooting commit for diff

---
+++ wall_filter_by_height.py
@@ -0,0 +1,25 @@
+"""
+Advanced collection of data: Collect all the walls at height of 10
+by iterating over all walls in project and checking parameter
+"""
+
+from System.Collections.Generic import List
+
+import Autodesk.Revit.DB as DB
+
+doc = __revit__.activeUIDocument.Document
+uidoc = __revit__.activeUIDocument
+
+walls = DB.FilteredElementCollector(doc) \
+        .OfCategory(DB.BuiltInCategory.OST_Walls) \
+        .WhereElementIsNotElementType() \
+        .ToElements()
+
+tallwalls_ids = []
+
+for wall in walls:
+    heightp = wall.LookupParameter('Unconnected Height')
+    if heightp and heightp.AsDouble() == 10.0:
+        tallwalls_ids.append(wall.Id)
+
+uidoc.Selection.SetElementIds(List[DB.ElementId](tallwalls_ids))
