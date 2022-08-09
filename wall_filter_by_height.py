35b2924f2d8037d535ec2f43da828e5a5888c5a1
pbull <17122363+peterbull@users.noreply.github.com>
2022-08-09 05:45:55 -0400

    Reverted to fixed 'slow' search script

--- wall_filter_by_height.py
+++ wall_filter_by_height.py
@@ -1,24 +1,25 @@
+"""
+Advanced collection of data: Collect all the walls at height of 10
+by iterating over all walls in project and checking parameter
+"""
+
 from System.Collections.Generic import List
 
 import Autodesk.Revit.DB as DB
 
-
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
-
 walls = DB.FilteredElementCollector(doc) \
-          .OfCategory(DB.BuiltInCategory.OST_Walls) \
-          .WhereElementIsNotElementType() \
-          .ToElements()
-
+        .OfCategory(DB.BuiltInCategory.OST_Walls) \
+        .WhereElementIsNotElementType() \
+        .ToElements()
 
 tallwalls_ids = []
 
 for wall in walls:
-    heightp = wall.Parameter[DB.BuiltInParameter.WALL_USER_HEIGHT_PARAM]
+    heightp = wall.LookupParameter('Unconnected Height')
     if heightp and heightp.AsDouble() == 10.0:
         tallwalls_ids.append(wall.Id)
-
 
 uidoc.Selection.SetElementIds(List[DB.ElementId](tallwalls_ids))
